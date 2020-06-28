c298b8608306ae0b2ba71b5b00466571039aed76
Lucas <lucasmeisl@hotmail.com>
2020-06-28 11:36:31 -0600

    Level Data Struct for simplified file read/write

--- Level.cpp
+++ Level.cpp
@@ -1,6 +1,84 @@
 #include "Level.hpp"
 
-//#include <fstream>
+#include <cassert>
+#include <sstream>
+#include <cstring>
+
+std::vector<Level> Level::m_levels;
+
+/*
+//128 bytes at a time
+struct LevelData
+{
+    uint8_t nameLength;
+    char name[45];
+    uint8_t numPieces;
+    uint8_t ids[15];
+    uint8_t width, height;
+    uint32_t data[16];
+};//THis seems to be great
+*/
+
+//save a file!
+void saveLevel(std::ofstream &file, const Level &level)
+{
+    LevelData L;
+    L.nameLength = level.name.length();
+    memcpy(L.name, level.name.c_str(), L.nameLength * sizeof(char));
+    //L.numPieces = ;
+    //L.ids[] = memcpy
+    L.width = level.width;
+    L.height = level.height;
+    memcpy(L.data, &level.data[0], sizeof(uint32_t) * level.data.size());
+
+    file.write((char*)&L, sizeof(LevelData));
+}
+
+Level::Level(const LevelData &L)
+{
+    name.reserve(L.nameLength);
+    memcpy(&name[0], L.name, L.nameLength * sizeof(char));
+    width = L.width;
+    height = L.height;
+    data.reserve(height);
+    memcpy(&data[0], L.data, sizeof(uint32_t) * height);
+}
+
+std::vector<Level> loadLevels(const std::string &fileName)
+{
+    std::ifstream file("Assets/" + fileName);
+    if(!file) throw std::runtime_error(fileName + " not found");
+    FileInfoHeader FH;
+    file.read((char*)&FH, sizeof(FH));
+    
+    std::vector<Level> levels;
+
+    LevelData L;
+    for(auto i = 0; i < FH.numLevels; ++i)
+    {
+        file.read((char*)&L, sizeof(LevelData));
+        if(!file) throw std::runtime_error("Unable to read in level");
+        levels.emplace_back(L);
+    }
+
+    return levels;
+}
+
+void saveLevels(const std::vector<Level> &levels, const std::string &fileName)
+{
+    std::ofstream file("Assets/" + fileName);
+    if(!file) throw std::runtime_error("Unable to write to " + fileName);
+    
+    FileInfoHeader FH;
+    FH.numLevels = levels.size();
+    file.write((char*)&FH, sizeof(FH));
+
+    for(auto &lvl : levels)
+    {
+        saveLevel(file, lvl);
+    }
+    file.close();
+}
 
 Level::Level() : width(0), height(0), data() {}
 
@@ -15,6 +93,38 @@ Level::Level(unsigned int id) : Level(id < m_preLoadedLevels.size() ?
 
 const std::vector<Level> Level::m_preLoadedLevels = 
 {
+{
+    "Create Level",
+    8,
+    8,
+    {
+    0b00011000,
+    0b00011000,
+    0b00011000,
+    0b11111111,
+    0b11111111,
+    0b00011000,
+    0b00011000,
+    0b00011000,
+    }
+},
+{
+    "Change Folders",
+    10,
+    10,
+    {
+    0b0000111111,
+    0b0000000001,
+    0b0011111101,
+    0b0000000101,
+    0b1111110101,
+    0b1111110101,
+    0b1111110100,
+    0b1111110100,
+    0b1111110000,
+    0b1111110000,
+    }
+},
 {
     "3 x 20 Rectangle",
     20,
@@ -62,7 +172,7 @@ const std::vector<Level> Level::m_preLoadedLevels =
     }
 },
 {
-    "8 x 8 Square with 2 x 2 Hole",
+    "8 x 8 Square\n(with 2 x 2 Hole)",
     8,
     8,
     {
@@ -77,7 +187,7 @@ const std::vector<Level> Level::m_preLoadedLevels =
     }
 },
 {
-    "8 x 8 Square with Corners Clipped",
+    "8 x 8 Square\n(with Corners Clipped)",
     8,
     8,
     {
@@ -92,7 +202,7 @@ const std::vector<Level> Level::m_preLoadedLevels =
     }
 },
 {
-    "Elephant (side view)",
+    "Elephant\n(side view)",
     10,
     11,
     {
@@ -143,7 +253,7 @@ const std::vector<Level> Level::m_preLoadedLevels =
     }
 },
 {
-    "Elephant Face (front view)",
+    "Elephant Face\n(front view)",
     11,
     8,
     {
@@ -188,7 +298,7 @@ const std::vector<Level> Level::m_preLoadedLevels =
     }
 },
 {
-    "Beetle (by Lucas)",
+    "Beetle\n(by Lucas)",
     12,
     8,
     {
@@ -219,10 +329,21 @@ const std::vector<Level> Level::m_preLoadedLevels =
     }
 },
 {
-    "Playground",
-    0,
-    0,
-    {}
+    "L Shape",
+    10,
+    10,
+    {
+    0b1111111110,
+    0b1111111110,
+    0b1111111110,
+    0b1110000000,
+    0b1110111110,
+    0b1110000000,
+    0b1110011000,
+    0b1110010010,
+    0b1110110111,
+    0b0000000010
+    }
 }
 };
 /*
--- Level.hpp
+++ Level.hpp
@@ -3,18 +3,53 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
 
-//just a bunch of numbers!
+//128 bytes at a time
+struct LevelData
+{
+    uint8_t nameLength;
+    char name[45];
+    uint8_t numPieces;
+    uint8_t ids[15];
+    uint8_t width, height;
+    uint32_t data[16];
+};
+//45 chars wide //can store as utf 8...
+//can fit 32 levels in a file, set all to -1 at the beginning! //fill with FF
+//fill 128 bytes for file size 4096 just because it is cool and I am bored and don't want to finish this game
+struct FileInfoHeader//62 bytes for the level info
+{//load the vector into memory
+//stays in memory
+//rewrite it out (if modified) again, use std::swap and shit
+//so don't worry . let the computer worry
+    uint8_t numLevels;//up to 256 levels in a file
+};//4096
+
+//am I modified???
+//is an option... or save it separately
+struct FileInfoFooter
+{
+    char hash[32];
+};
+
 struct Level
 {
     Level();
     Level(const std::string &s, unsigned int width, unsigned int height, std::vector<uint32_t> data);
     Level(unsigned int id);
-    const std::string name;
-    const unsigned int width, height;
-    const std::vector<uint32_t> data;
+    Level(const LevelData &L);
+    std::string name;
+    unsigned int width, height;
+    std::vector<uint32_t> data;//unconstified!!!
 //private:
     static const std::vector<Level> m_preLoadedLevels;
+    static std::vector<Level> m_levels;
 };
+
+std::vector<Level> loadLevels(const std::string &fileName);//to do test rvo??? return value optimization
+void saveLevels(const std::vector<Level> &levels);
+void saveLevel(std::ofstream &file, const Level &level);
+Level readLevel(std::ifstream &file);
 
 #endif //PENTAMINO_LEVEL_HPP
