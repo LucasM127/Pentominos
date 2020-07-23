4eb4a06ffca0e07c083dc396fcce7f00747cb5c4
Lucas <lucasmeisl@hotmail.com>
2020-07-22 21:18:50 -0600

    Added Help Page

---
+++ HelpState.hpp
@@ -0,0 +1,22 @@
+#ifndef HELPSTATE_HPP
+#define HELPSTATE_HPP
+
+#include "GameState.hpp"
+
+class HelpState : public GameState
+{
+public:
+    HelpState(StateMgr &mgr, Context &context);
+    void handleEvent(const sf::Event &event) override;
+    void render() override;
+private:
+    //bunch of texts
+    std::vector<sf::Text> m_texts;
+    sf::Text m_instructionText;
+    sf::Sprite sprite;
+    sf::Texture texture;
+
+    void position();
+};
+
+#endif //HELPSTATE_HPP
